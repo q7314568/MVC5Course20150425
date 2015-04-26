2feb8d239dcdaf4214013a2f598308f5a7578019
q7314568 <q7314568@gmail.com>
2015-04-26 09:44:16 +0800

    練習 FileResult 的各種用法 ( 檔案路徑, 指定 Content-Type, byte[], 強制下載檔案 )

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +13,38 @@ namespace MVC5Course.Controllers
         public ActionResult Index()
         {
             return View("View1");
+        }
+        public ActionResult FileDownload1()
+        {
+            Byte[] content = System.IO.File.ReadAllBytes(Server.MapPath("~/content/love-bird.jpg"));
+
+            return File(content,"image/jpeg");
+        }
+        public ActionResult FileDownload2()
+        {
+            //Byte[] content = System.IO.File.ReadAllBytes(Server.MapPath("~/content/love-bird.jpg"));
+
+            return File(Server.MapPath("~/content/love-bird.jpg"), "image/jpeg");
+        }
+
+        public ActionResult FileDownload3()
+        {
+            //Byte[] content = System.IO.File.ReadAllBytes(Server.MapPath("~/content/love-bird.jpg"));
+            
+            var wc =new WebClient();
+            var content= wc.DownloadData("http://upload.wikimedia.org/wikipedia/commons/0/03/Mountain_Bluebird.jpg");
+
+            return File(content, "image/jpeg");
+        }
+
+        public ActionResult FileDownload4()
+        {
+            //Byte[] content = System.IO.File.ReadAllBytes(Server.MapPath("~/content/love-bird.jpg"));
+
+            var wc = new WebClient();
+            var content = wc.DownloadData("http://upload.wikimedia.org/wikipedia/commons/0/03/Mountain_Bluebird.jpg");
+
+            return File(content, "text/plain");
         }
     }
 }
---
+++ MVC5Course/Controllers/jQueryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class jQueryController : Controller
+    {
+        // GET: jQuery
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult etc()
+        {
+            return View();
+        }
+    }
+}
