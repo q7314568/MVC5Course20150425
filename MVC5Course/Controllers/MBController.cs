ad9f2d0f3ef22c3ec37957af0d677d9970372787
q7314568 <q7314568@gmail.com>
2015-04-26 14:07:54 +0800

    練習簡單模型繫結 (string, FormCollection) 與 複雜模型繫結 (Simple1ViewModel)

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC5Course.Models.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,6 +27,44 @@ namespace MVC5Course.Controllers
                 string aa= s.ToString();
             }
             return View();
+        }
+
+        public ActionResult SimpleBind1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SimpleBind1(string UserName,string Password)
+        {
+            ViewBag.result = "SimpleBind1-UserName : " + UserName + ",Password : " + Password;
+            return View("ShowBindResult");
+        }
+
+
+        public ActionResult SimpleBind2()
+        {
+            return View("SimpleBind1");
+        }
+
+        [HttpPost]
+        public ActionResult SimpleBind2(FormCollection Form)
+        {
+            ViewBag.result = "SimpleBind2-UserName : " + Form["UserName"] + ",Password : " + Form["Password"];
+            return View("ShowBindResult");
+        }
+
+
+        public ActionResult ComplexBind1()
+        {
+            return View("SimpleBind1");
+        }
+
+        [HttpPost]
+        public ActionResult ComplexBind1(SimpleViewModel svm)
+        {
+            ViewBag.result = "ComplexBind1-UserName : " + svm.UserName + ",Password : " + svm.Password;
+            return View("ShowBindResult");
         }
     }
 }
---
+++ MVC5Course/Models/ViewModel/SimpleViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models.ViewModel
+{
+    public class SimpleViewModel
+    {
+        [Required]
+        [Display(Name="使用者名稱")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "密碼")]
+
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        [Display(Name = "密碼核對")]
+
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [Display(Name = "年齡")]
+
+        public int age { get; set; }
+    }
+}
